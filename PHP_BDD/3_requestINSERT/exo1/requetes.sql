3a84bf5218da8e7077b896d8d271308e581f1877
Madeline Ricateau <madeline.ricateau@outlook.fr>
2025-01-20 15:58:09 +0100

    Suppression de l'ancien dossier PHP_BDD

--- PHP_BDD/3_requestINSERT/exo1/requetes.sql
+++
@@ -1,3 +0,0 @@
--- Insertion de données
-INSERT INTO tests.famille_table(nom, prenom, statut, date_naissance) 
-VALUES('$nom', '$prenom', '$statut', '$date_naissance');
--- PHP_BDD/4_requestUPDATE/exo1/requestUpdate.sql
+++
@@ -1,5 +0,0 @@
--- Mise à jour de données
-UPDATE tests.famille_table
-SET date_naissance="1964-07-20"
-WHERE prenom="Pavetta"
-AND nombre="Fiona Riannon";
