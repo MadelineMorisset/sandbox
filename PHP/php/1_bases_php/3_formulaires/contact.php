5187da7fbc4f08239a1b324ea0199c4f8afcf2d5
Madeline Ricateau <madeline.ricateau@outlook.fr>
2025-01-21 14:18:52 +0100

    Gestion de formulaires en PHP

---
+++ PHP/php/1_bases_php/3_formulaires/contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formulaires</title>
+</head>
+
+<body>
+  <form action="destination.php" method="$_GET">
+    Prénom : <input type="text" name="prenom">
+    <input type="submit" value="Envoyer">
+  </form>
+</body>
+
+</html>
